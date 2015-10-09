f102fb7e3a810e61c2ad751be3b5855ab0905db1
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-10-09 15:00:34 +0300

    Remove derived instances

--- src/Control/Replay/Class.hs
+++ src/Control/Replay/Class.hs
@@ -7,13 +7,6 @@
 module Control.Replay.Class where
 
 import Control.Applicative
-import Control.Monad.Free
-import Control.Comonad.Cofree
-
-import Data.Functor.Compose
-import Data.Functor.Constant
-import Data.Functor.Identity
-import Data.Functor.Product
 
 import GHC.Generics
 
@@ -25,20 +18,6 @@ class Replay f g where
 
 instance Replay ((->) a) ((,) a) where
   replay f g (x, y) = (,) x <$> f (g x) y
-
-instance Replay Identity Identity
-instance Eq m => Replay (Const m) (Const m)
-instance Eq m => Replay (Constant m) (Constant m)
-instance (Functor f, Functor f', Functor g, Functor g', Replay f g, Replay f' g') => Replay (Compose f f') (Compose g g')
-instance (Replay f g, Replay f' g') => Replay (Product f f') (Product g g')
-instance (Functor f, Functor g, Replay f g) => Replay (Cofree f) (Cofree g)
-instance (Functor f, Functor g, Replay f g) => Replay (Free f) (Free g)
-
-deriving instance Functor f => Generic1 (Free f)
-deriving instance Functor f => Generic1 (Cofree f)
-deriving instance (Functor f, Functor g) => Generic1 (Compose f g)
-deriving instance Generic1 (Product f g)
-deriving instance Generic1 (Constant m)
 
 class GReplay f g where
   greplay :: Alternative m => (a -> b -> m c) -> f a -> g b -> m (g c)
